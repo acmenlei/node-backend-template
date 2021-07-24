6ddf621b9b0fcff6c9cf6919ac783293fa83b145
codeleilei <972761675@qq.com>
2021-07-24 14:06:24 +0800

    adjust: adjust the format of the code

--- src/models/User.js
+++ src/models/User.js
@@ -1,16 +1,40 @@
-const { sequelize } = require('../connect/mysql');
-const { Model, DataTypes } = require('sequelize');
+const { sequelize } = require("../connect/mysql");
+const { Model, DataTypes } = require("sequelize");
 
-class User extends Model { }
+class User extends Model {}
 
-User.init({
-    ll_id: { type: DataTypes.INTEGER, primaryKey: true, autoIncrement: false },
-    ll_username: { type: DataTypes.STRING, primaryKey: true, autoIncrement: false },
-    ll_password: { type: DataTypes.TEXT },
-    ll_email: { type: DataTypes.STRING },
-    ll_sex: { type: DataTypes.CHAR },
-    ll_createdTime: { type: DataTypes.DATE },
-    ll_updatedTime: { type: DataTypes.DATE },
-}, { tableName: 'll_users', sequelize })
+User.init(
+  {
+    ll_id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    ll_username: {
+      type: DataTypes.STRING,
+      primaryKey: true,
+      autoIncrement: false,
+    },
+    ll_password: {
+      type: DataTypes.TEXT,
+    },
+    ll_email: {
+      type: DataTypes.STRING,
+    },
+    ll_sex: {
+      type: DataTypes.CHAR,
+    },
+    ll_createdTime: {
+      type: DataTypes.DATE,
+    },
+    ll_updatedTime: {
+      type: DataTypes.DATE,
+    },
+  },
+  {
+    tableName: "ll_users",
+    sequelize,
+  }
+);
 
-module.exports = User
+module.exports = User;
